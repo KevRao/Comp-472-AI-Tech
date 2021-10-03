d6826d32bbc6d73808f0695b383e2c8139df4443
KevRao <kevkevrao@gmail.com>
2021-10-03 16:42:41 -0400

    Step 5, split into training and testing data.

--- Kevin_Task1.py
+++ Kevin_Task1.py
@@ -18,15 +18,17 @@ import numpy as np;
 import matplotlib.pyplot as plt;
 from collections import Counter;
 import collections;
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer;
+from sklearn.model_selection import train_test_split;
 
 #%% Helper Functions Declaration
 #Retrieve data from the corpus
 def getCorpus(corpus_directory):
     #Encoding as specified in the mini-project document.
     corpus_encoding = 'latin1'
+    description = "Corpus of BBC articles. "
 
-    return datasets.load_files(corpus_directory, encoding=corpus_encoding)
+    return datasets.load_files(corpus_directory, encoding=corpus_encoding, description=description)
 
 #Handle the data from the corpus
 #In this case, determine the corpus distribution by classification
@@ -45,7 +47,8 @@ def determineCorpusDistribution(corpus):
     
     return effectif, indexes, labels
 
-def determineCorpusDetails_byClassification(corpus):
+#Optional vocabulary should be given for train/test corpuses, such that it matches the whole, entire corpus.
+def determineCorpusDetails_byClassification(corpus, vocabulary=None):
     #Organize the corpus data into its classifications
     #Structure is a list in a dict in a dict
     corpus_collections = collections.defaultdict(lambda: collections.defaultdict(list));
@@ -56,20 +59,68 @@ def determineCorpusDetails_byClassification(corpus):
     #Initiate countVectorizer for each classification
     for corpus_collection in corpus_collections.values():
         #create a new instance of countVectorizer
-        collection_vectorizer = CountVectorizer()
+        collection_vectorizer = CountVectorizer(vocabulary=vocabulary)
         #attach the new countVectorizer's reference.
         corpus_collection['vectorizer'].append(collection_vectorizer)
         #process the countVectorizer to the data
-        collection_vectorizer.fit_transform(corpus_collection['data'])
+        corpus_collection['document-term'] = collection_vectorizer.fit_transform(corpus_collection['data'])
     
     #do the same, but for the single instance of the whole corpus
-    whole_vectorizer = CountVectorizer()
+    whole_vectorizer = CountVectorizer(vocabulary=vocabulary)
     corpus_collections['*Whole']['data'] = corpus.data
     corpus_collections['*Whole']['vectorizer'] = whole_vectorizer
-    whole_vectorizer.fit_transform(corpus.data)
+    corpus_collections['*Whole']['document-term'] = whole_vectorizer.fit_transform(corpus.data)
     
     return corpus_collections
 
+#Split and return the data&labels into train and test lists.
+def splitTrainTestData(data, labels, train_size_proportion):
+    #Random_State as specified in the mini-project document.
+    random_state = None
+    
+    #Split and shuffle according to given proportion.
+    #can just be returned directly, but this helps readability of what the output variables are (since there're 4 of them).
+    training_data, testing_data, training_labels, testing_labels = train_test_split(data, labels, train_size = train_size_proportion, random_state=random_state)
+    return training_data, testing_data, training_labels, testing_labels
+
+#Split and return the corpus into train and test corpuses.
+def splitTrainTestData_2(corpus, train_size_proportion):
+    #Random_State as specified in the mini-project document.
+    random_state = None
+    
+    data_index_selection = labels_index_selection = range(len(corpus.data))
+    split_shuffled_indexes = train_test_split(data_index_selection, labels_index_selection, train_size = train_size_proportion, random_state=random_state)
+    # if (   split_shuffled_indexes[0] != split_shuffled_indexes[2]
+    #     or split_shuffled_indexes[1] != split_shuffled_indexes[3]):
+    #     raise AssertionError("Somehow train_test_split did not return corresponding splits for train/test.")
+    assert  split_shuffled_indexes[0] == split_shuffled_indexes[2] \
+        and split_shuffled_indexes[1] == split_shuffled_indexes[3] \
+        , "Somehow train_test_split did not return corresponding splits for train/test."
+    
+    #try_corpus = {split_corpus for data_index, split_corpus in enumerate(corpus) if data_index in split_shuffled_indexes[0]}
+    #Copy over the corpus' entries for the corresponding indices. For both the train and test lists.
+    train_corpus = collections.defaultdict(list)
+    for data_index in split_shuffled_indexes[0]:
+        train_corpus["data"     ].append(corpus.data     [data_index])
+        train_corpus["filenames"].append(corpus.filenames[data_index])
+        train_corpus["target"   ].append(corpus.target   [data_index])
+    train_corpus["DESCR"] = corpus.DESCR + "Training data. "
+    train_corpus["target_names"] = corpus.target_names
+    
+    test_corpus = collections.defaultdict(list)
+    for data_index in split_shuffled_indexes[1]:
+        test_corpus["data"     ].append(corpus.data     [data_index])
+        test_corpus["filenames"].append(corpus.filenames[data_index])
+        test_corpus["target"   ].append(corpus.target   [data_index])
+    test_corpus["DESCR"] = corpus.DESCR + "Testing data. "
+    test_corpus["target_names"] = corpus.target_names
+    #Split and shuffle according to given proportion.
+    #can just be returned directly, but this helps readability of what the output variables are (since there're 4 of them).
+    #return training_data, testing_data, training_labels, testing_labels
+    return train_corpus, test_corpus
+    
+    
+    
 #TODO: remove either of the generateBarGraph methods. Only one is needed. Choice between is in axis label order.
 
 #Make a bar plot out of the corpus distribution.
@@ -117,6 +168,7 @@ def getFigureCount():
 script_directory = os.path.dirname(__file__)
 #Read
 BBC_directory = os.path.join(script_directory, 'BBC')
+train_size_proportion = 0.80
 #Write
 output_directory = os.path.join(script_directory, 'output')
 #track the number of figures created
@@ -138,6 +190,12 @@ def main():
     print("Done! Output is located in:", output_directory, ".")
     #Step 4 part 1
     corpus_by_classification = determineCorpusDetails_byClassification(corpus)
+    corpus_vocabulary = corpus_by_classification["*Whole"]["vectorizer"].vocabulary_
+    #Step 5
+    split_train_test_corpuses = splitTrainTestData_2(corpus, train_size_proportion)
+    
+    #train_corpus_by_classification = determineCorpusDetails_byClassification(split_train_test_corpuses[0], corpus_vocabulary)
+    print("Done! Again.")
 
 if __name__ == "__main__":
     begin_time = time.perf_counter()
