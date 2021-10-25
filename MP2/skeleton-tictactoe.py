e49f80981fd33b39a570553f3bad893e6cf18f49
KevRao <kevkevrao@gmail.com>
2021-10-24 20:33:30 -0400

    change board size in memory, replace strings with constants.

--- MP2/skeleton-tictactoe.py
+++ MP2/skeleton-tictactoe.py
@@ -2,59 +2,72 @@
 
 import time
 
+import numpy as np
+
 class Game:
     MINIMAX = 0
     ALPHABETA = 1
     HUMAN = 2
     AI = 3
     
-    def __init__(self, recommend = True):
+    #In-Game Notation
+    CROSS  = 'X'
+    NOUGHT = 'O'
+    EMPTY  = '.'
+    
+    def __init__(self, recommend = True, board_size = 4):
+        self.board_size = board_size
+        
         self.initialize_game()
         self.recommend = recommend
         
     def initialize_game(self):
-        self.current_state = [['.','.','.'],
-                              ['.','.','.'],
-                              ['.','.','.']]
+        self.current_state = np.full((self.board_size , self.board_size ), self.EMPTY, 'str')
         # Player X always plays first
-        self.player_turn = 'X'
+        self.player_turn = self.CROSS
 
     def draw_board(self):
         print()
-        for y in range(0, 3):
-            for x in range(0, 3):
-                print(F'{self.current_state[x][y]}', end="")
-            print()
+        print('\n'.join([''.join([cell for cell in row]) for row in self.current_state]))
         print()
         
     def is_valid(self, px, py):
-        if px < 0 or px > 2 or py < 0 or py > 2:
+        #invalid if there's a coordinate not in the board.
+        if not 0 < px < self.board_size or 0 < py < self.board_size:
             return False
-        elif self.current_state[px][py] != '.':
+        #invalid if 
+        elif self.current_state[px][py] != self.EMPTY:
             return False
         else:
             return True
 
     def is_end(self):
+        #TODO: convert this to general case.
+            
+        # for player in [self.CROSS, self.NOUGHT]:
+            
         # Vertical win
+        # for row in range(self.board_size):
+        #     if self.current_state[]
+        
         for i in range(0, 3):
-            if (self.current_state[0][i] != '.' and
+            if (self.current_state[0][i] != self.EMPTY and
                 self.current_state[0][i] == self.current_state[1][i] and
                 self.current_state[1][i] == self.current_state[2][i]):
                 return self.current_state[0][i]
         # Horizontal win
         for i in range(0, 3):
-            if (self.current_state[i] == ['X', 'X', 'X']):
-                return 'X'
-            elif (self.current_state[i] == ['O', 'O', 'O']):
-                return 'O'
+            if (np.all(self.current_state[i] == [self.CROSS] * self.board_size)):
+                return self.CROSS
+            elif (np.all(self.current_state[i] == [self.NOUGHT] * self.board_size)):
+                return self.NOUGHT
         # Main diagonal win
-        if (self.current_state[0][0] != '.' and
+        if (self.current_state[0][0] != self.EMPTY and
             self.current_state[0][0] == self.current_state[1][1] and
             self.current_state[0][0] == self.current_state[2][2]):
             return self.current_state[0][0]
         # Second diagonal win
-        if (self.current_state[0][2] != '.' and
+        if (self.current_state[0][2] != self.EMPTY and
             self.current_state[0][2] == self.current_state[1][1] and
             self.current_state[0][2] == self.current_state[2][0]):
             return self.current_state[0][2]
@@ -62,20 +75,20 @@ class Game:
         for i in range(0, 3):
             for j in range(0, 3):
                 # There's an empty field, we continue the game
-                if (self.current_state[i][j] == '.'):
+                if (self.current_state[i][j] == self.EMPTY):
                     return None
         # It's a tie!
-        return '.'
+        return self.EMPTY
 
     def check_end(self):
         self.result = self.is_end()
         # Printing the appropriate message if the game has ended
         if self.result != None:
-            if self.result == 'X':
+            if self.result == self.CROSS:
                 print('The winner is X!')
-            elif self.result == 'O':
+            elif self.result == self.NOUGHT:
                 print('The winner is O!')
-            elif self.result == '.':
+            elif self.result == self.EMPTY:
                 print("It's a tie!")
             self.initialize_game()
         return self.result
@@ -91,10 +104,10 @@ class Game:
                 print('The move is not valid! Try again.')
 
     def switch_player(self):
-        if self.player_turn == 'X':
-            self.player_turn = 'O'
-        elif self.player_turn == 'O':
-            self.player_turn = 'X'
+        if self.player_turn == self.CROSS:
+            self.player_turn = self.NOUGHT
+        elif self.player_turn == self.NOUGHT:
+            self.player_turn = self.CROSS
         return self.player_turn
 
     def minimax(self, max=False):
@@ -110,30 +123,30 @@ class Game:
         x = None
         y = None
         result = self.is_end()
-        if result == 'X':
+        if result == self.CROSS:
             return (-1, x, y)
-        elif result == 'O':
+        elif result == self.NOUGHT:
             return (1, x, y)
-        elif result == '.':
+        elif result == self.EMPTY:
             return (0, x, y)
         for i in range(0, 3):
             for j in range(0, 3):
-                if self.current_state[i][j] == '.':
+                if self.current_state[i][j] == self.EMPTY:
                     if max:
-                        self.current_state[i][j] = 'O'
+                        self.current_state[i][j] = self.NOUGHT
                         (v, _, _) = self.minimax(max=False)
                         if v > value:
                             value = v
                             x = i
                             y = j
                     else:
-                        self.current_state[i][j] = 'X'
+                        self.current_state[i][j] = self.CROSS
                         (v, _, _) = self.minimax(max=True)
                         if v < value:
                             value = v
                             x = i
                             y = j
-                    self.current_state[i][j] = '.'
+                    self.current_state[i][j] = self.EMPTY
         return (value, x, y)
 
     def alphabeta(self, alpha=-2, beta=2, max=False):
@@ -149,30 +162,30 @@ class Game:
         x = None
         y = None
         result = self.is_end()
-        if result == 'X':
+        if result == self.CROSS:
             return (-1, x, y)
-        elif result == 'O':
+        elif result == self.NOUGHT:
             return (1, x, y)
-        elif result == '.':
+        elif result == self.EMPTY:
             return (0, x, y)
         for i in range(0, 3):
             for j in range(0, 3):
-                if self.current_state[i][j] == '.':
+                if self.current_state[i][j] == self.EMPTY:
                     if max:
-                        self.current_state[i][j] = 'O'
+                        self.current_state[i][j] = self.NOUGHT
                         (v, _, _) = self.alphabeta(alpha, beta, max=False)
                         if v > value:
                             value = v
                             x = i
                             y = j
                     else:
-                        self.current_state[i][j] = 'X'
+                        self.current_state[i][j] = self.CROSS
                         (v, _, _) = self.alphabeta(alpha, beta, max=True)
                         if v < value:
                             value = v
                             x = i
                             y = j
-                    self.current_state[i][j] = '.'
+                    self.current_state[i][j] = self.EMPTY
                     if max: 
                         if value >= beta:
                             return (value, x, y)
@@ -198,22 +211,22 @@ class Game:
                 return
             start = time.time()
             if algo == self.MINIMAX:
-                if self.player_turn == 'X':
+                if self.player_turn == self.CROSS:
                     (_, x, y) = self.minimax(max=False)
                 else:
                     (_, x, y) = self.minimax(max=True)
             else: # algo == self.ALPHABETA
-                if self.player_turn == 'X':
+                if self.player_turn == self.CROSS:
                     (m, x, y) = self.alphabeta(max=False)
                 else:
                     (m, x, y) = self.alphabeta(max=True)
             end = time.time()
-            if (self.player_turn == 'X' and player_x == self.HUMAN) or (self.player_turn == 'O' and player_o == self.HUMAN):
+            if (self.player_turn == self.CROSS and player_x == self.HUMAN) or (self.player_turn == self.NOUGHT and player_o == self.HUMAN):
                 if self.recommend:
                     print(F'Evaluation time: {round(end - start, 7)}s')
                     print(F'Recommended move: x = {x}, y = {y}')
                 (x,y) = self.input_move()
-            if (self.player_turn == 'X' and player_x == self.AI) or (self.player_turn == 'O' and player_o == self.AI):
+            if (self.player_turn == self.CROSS and player_x == self.AI) or (self.player_turn == self.NOUGHT and player_o == self.AI):
                 print(F'Evaluation time: {round(end - start, 7)}s')
                 print(F'Player {self.player_turn} under AI control plays: x = {x}, y = {y}')
             self.current_state[x][y] = self.player_turn
