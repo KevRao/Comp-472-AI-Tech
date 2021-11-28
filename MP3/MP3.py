4388837440f7b458cd423150e654ffdfe7566d1f
KevRao <kevkevrao@gmail.com>
2021-11-28 00:00:11 -0500

    [Refactor] Organize into methods.

--- MP3/MP3.py
+++ MP3/MP3.py
@@ -18,65 +18,93 @@ import csv
 #Write
 local_directory = os.path.dirname(__file__)
 output_directory = os.path.join(local_directory, 'output')
-#%% Init embeddings
-analysis = []
-model_name = "word2vec-google-news-300"
-start = time.perf_counter()
-wv = api.load(model_name)
-end = time.perf_counter()
 
-print("Elapsed time:", end-start)
 #%% Task 1
+def get_model(model_name):
+	start = time.perf_counter()
+	wv = api.load(model_name)
+	end = time.perf_counter()
 
-#Read questionnaire file
-file = pd.read_csv("synonyms.csv")
-answers = file["answer"].values
-questions = file["question"].values
+	print(f"Elapsed time to get {model_name}: {end-start}s.")
+	return wv
 
-#initialize the lists.
-#List comprehension is doable, but very messy to declare variables inside.
-guess_words  = []
-guess_labels = []
-for question, answer, *choices_all in file.values:
- 	#exclude options that the model doesn't know about.
- 	choices = [choice for choice in choices_all if wv.has_index_for(choice)]
- 	#is a guess if there are no choices, or the model doesn't know the question-word
- 	is_valid = bool(choices) and wv.has_index_for(question)
+def get_synonym_test():
+	#Read questionnaire file
+	file = pd.read_csv("synonyms.csv")
+	return file
 
- 	#if the question word exists, pick the most similar choice,
- 	# otherwise, pick randomly from the valid choices,
- 	# otherwise, pick randomly from all the choices.
- 	guess_word = wv.most_similar_to_given(question, choices) if is_valid else random.choice(choices) if choices else random.choice(choices_all)
- 	#remember the guess words, and whether it was determined through the model or random.
- 	guess_words.append(guess_word)
- 	guess_labels.append(not is_valid)
+def details_analysis(synonym_test, keyedVectors, model_name):
+	file = synonym_test
+	wv = keyedVectors
 
-correct_labels = answers == guess_words
+	answers = file["answer"].values
+	questions = file["question"].values
 
-labels = np.full_like(guess_words, "wrong")
-labels[correct_labels] = "correct"
-labels[guess_labels]   = "guess"
+	#initialize the lists.
+	#List comprehension is doable, but very messy to declare variables inside.
+	guess_words  = []
+	guess_labels = []
+	for question, answer, *choices_all in file.values:
+		#exclude options that the model doesn't know about.
+		choices = [choice for choice in choices_all if wv.has_index_for(choice)]
+		#is a guess if there are no choices, or the model doesn't know the question-word
+		is_valid = bool(choices) and wv.has_index_for(question)
 
-detail = np.array([questions, answers, guess_words, labels]).T
+		#if the question word exists, pick the most similar choice,
+		# otherwise, pick randomly from the valid choices,
+		# otherwise, pick randomly from all the choices.
+		guess_word = wv.most_similar_to_given(question, choices) if is_valid else random.choice(choices) if choices else random.choice(choices_all)
+		#remember the guess words, and whether it was determined through the model or random.
+		guess_words.append(guess_word)
+		guess_labels.append(not is_valid)
 
-vocab_size = len(wv.index_to_key)
-correct = np.count_nonzero(correct_labels)
-valid = np.count_nonzero(~np.array(guess_labels))
-acc = correct / valid
-performance = [model_name, vocab_size, correct, valid, acc]
-analysis.append(performance)
+	correct_labels = answers == guess_words
 
-#Task 1 output details
-filename = f"{model_name}-details.csv"
-filepath = os.path.join(output_directory, filename)
-with open(filepath, 'w', newline='') as output_file:
-	writer = csv.writer(output_file)
-	writer.writerows(detail)
+	labels = np.full_like(guess_words, "wrong")
+	labels[correct_labels] = "correct"
+	labels[guess_labels]   = "guess"
 
-#Task 1 output analysis
-filename = "analysis.csv"
-filepath = os.path.join(output_directory, filename)
-with open(filepath, 'w', newline='') as output_file:
-	writer = csv.writer(output_file)
-	writer.writerows(analysis)
+	detail = np.array([questions, answers, guess_words, labels]).T
 
+	vocab_size = len(wv.index_to_key)
+	correct = np.count_nonzero(correct_labels)
+	valid = np.count_nonzero(~np.array(guess_labels))
+	acc = correct / valid
+	performance = [model_name, vocab_size, correct, valid, acc]
+
+	return detail, performance
+
+def output_csv(output_filename, table):
+	#Task 1 output details
+	filename = output_filename#f"{model_name}-details.csv"
+	filepath = os.path.join(output_directory, filename)
+	with open(filepath, 'w', newline='') as output_file:
+		writer = csv.writer(output_file)
+		writer.writerows(table)
+
+# %% Main flow
+def main():
+	analysis = []
+	model_names = ["word2vec-google-news-300"]
+
+	synonym_test = get_synonym_test()
+	#Batch Task1 and Task 2 together.
+	for model_name in model_names:
+		wv = get_model(model_name)
+		detail, performance = details_analysis(synonym_test, wv, model_name)
+
+		#Step 1, output details
+		filename = f"{model_name}-details.csv"
+		output_csv(filename, detail)
+		analysis.append(performance)
+
+	#Batch Step 2, output analysis
+	filename = "analysis.csv"
+	output_csv(filename, analysis)
+
+
+if __name__ == "__main__":
+    begin_time = time.perf_counter()
+    main()
+    end_time = time.perf_counter()
+    print("This script has taken", end_time - begin_time, "seconds to execute.")
