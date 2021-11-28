72ef8bc47b84293ea6228406657f1e0368c9f7a6
KevRao <kevkevrao@gmail.com>
2021-11-27 23:39:20 -0500

    move around and cleanup

--- MP3/MP3.py
+++ MP3/MP3.py
@@ -19,15 +19,20 @@ import csv
 local_directory = os.path.dirname(__file__)
 output_directory = os.path.join(local_directory, 'output')
 #%% Init embeddings
+analysis = []
+model_name = "word2vec-google-news-300"
 start = time.perf_counter()
-wv = api.load('word2vec-google-news-300')
-print("help")
+wv = api.load(model_name)
 end = time.perf_counter()
 
 print("Elapsed time:", end-start)
 #%% Task 1
 
+#Read questionnaire file
 file = pd.read_csv("synonyms.csv")
+answers = file["answer"].values
+questions = file["question"].values
+
 #initialize the lists.
 #List comprehension is doable, but very messy to declare variables inside.
 guess_words  = []
@@ -46,28 +51,14 @@ for question, answer, *choices_all in file.values:
  	guess_words.append(guess_word)
  	guess_labels.append(not is_valid)
 
-#the same, but with list comprehension
-# guess_words, guess_labels = zip(*[(wv.most_similar_to_given(question, choices), not is_valid) if is_valid else (random.choice(choices) if choices else random.choice(choices_all), not is_valid)
-# 			   for question, answer, choices_all, choices, is_valid in [(question, answer, choices_all, choices, bool(choices and wv.has_index_for(question)))
-# 			   for question, answer, choices_all, choices in [(question, answer, choices_all, [choice for choice in choices_all if wv.has_index_for(choice)])
-# 			   for question, answer, *choices_all in file.values]]])
-# # guess_words = np.array(guess_words)
-# # guess_labels = np.array(guess_labels)
-# guess_words, guess_labels =  list(guess_words), list(guess_labels)
-
-answers = file["answer"].values
 correct_labels = answers == guess_words
-# guess_labels = guess_words == None
 
 labels = np.full_like(guess_words, "wrong")
 labels[correct_labels] = "correct"
 labels[guess_labels]   = "guess"
 
-questions = file["question"].values
 detail = np.array([questions, answers, guess_words, labels]).T
 
-analysis = []
-model_name = "word2vec-google-news-300"
 vocab_size = len(wv.index_to_key)
 correct = np.count_nonzero(correct_labels)
 valid = np.count_nonzero(~np.array(guess_labels))
