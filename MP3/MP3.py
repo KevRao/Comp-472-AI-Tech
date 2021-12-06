8bbc8d2ff3c1bde467afff5545a8154c7f90d067
KevRao <kevkevrao@gmail.com>
2021-12-05 20:34:21 -0500

    Graphing the analysis

--- MP3/MP3.py
+++ MP3/MP3.py
@@ -10,12 +10,22 @@ import random
 import pandas as pd
 import numpy as np
 import gensim.downloader as api
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 import csv
 
 #%% Config
 #Read
 model_names = ['word2vec-google-news-300', 'glove-twitter-200', 'glove-wiki-gigaword-200', 'glove-wiki-gigaword-50', 'glove-wiki-gigaword-300']#["GensimContinuousSkipgram-BritishNationalCorpus-300"]#["word2vec-google-news-300"]
+
+#Model accuracy standards
+gold_standard_mean = 0.8557
+gold_standard_std  = 0.1318
+gold_standard_top  = gold_standard_mean + gold_standard_std
+gold_standard_bot  = gold_standard_mean - gold_standard_std
+
+random_baseline = 0.25
 
 #Write
 local_directory = os.path.dirname(__file__)
@@ -98,6 +108,68 @@ def output_csv(output_filename, table):
 		writer = csv.writer(output_file)
 		writer.writerows(table)
 
+#Adapter to function generateAnalysisBarGraph_.
+def generateAnalysisBarGraph(analysis):
+	title = "Accuracy of Various Models"
+
+	#adapt analysis into data that's easier to parse, then extract relevant info.
+	data = list(zip(*analysis))
+	xlabels = data[0]
+	y = data[-1]
+	y_indexes = list(range(len(y)))
+
+	#call the actual function.
+	generateAnalysisBarGraph_(title, xlabels, y, y_indexes)
+
+#Make a combo plot out of the analysis.
+def generateAnalysisBarGraph_(title, labels, values, value_indexes):
+	#File format as specified in the mini-project document.
+	file_format = "pdf"
+	file_extension = "." + file_format
+
+	#figure number hard-coded to 1.
+	plot_title = f"Figure {1}. {title}."
+	plot_size = (8, 8)
+
+	#plot the model data as a bar graph.
+	plt.bar(value_indexes, values, tick_label=labels, color="xkcd:battleship grey")
+	#adjust the x-axis labels to be more readable.
+	plt.gcf().autofmt_xdate()
+	#write out the various labels of the graph
+	plt.title(plot_title)
+	plt.xlabel("Models")
+	plt.ylabel("Accuracy of Model")
+	#adjust the y-axis to include the 100% accuracy mark.
+	plt.gca().set_ylim([None, 1])
+	#adjust step size of ticks on y-axis.
+	ytick_spacing = 0.05
+	plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(ytick_spacing))
+
+	#linestyle
+	#https://matplotlib.org/stable/gallery/lines_bars_and_markers/linestyles.html
+	loosely_dashed = (0, (5, 10))
+
+	#plot the constant lines.
+	dashedline_color = "xkcd:cerulean"
+	plt.axhline(y=random_baseline   , label="Random baseline", color = "xkcd:red")
+	plt.axhline(y=gold_standard_mean, label="Gold standard mean", color="xkcd:light royal blue")
+	plt.axhline(y=gold_standard_top , label="Gold standard one standard deviation away", color=dashedline_color, linestyle=loosely_dashed)
+	plt.axhline(y=gold_standard_bot , color=dashedline_color, linestyle=loosely_dashed)
+	#plot the area of uncertainty as a see-through span.
+	plt.axhspan(gold_standard_top, gold_standard_bot, label="Gold standard within standard deviation", color="xkcd:robin's egg", alpha=0.3)
+
+	#provide text values for each bar
+	for count, value in enumerate(values):
+		#value and count used as coordinates for the text.
+		plt.text(value_indexes[count], value, f"{value:.2%}", va="top", ha="center", color="aliceblue")
+
+	#add legend in a place that is out of the way.
+	legend = plt.gca().legend(loc='lower left', shadow=True)
+	#change the size.
+	plt.gcf().set_size_inches(plot_size)
+	plt.savefig(os.path.join(output_directory, title + file_extension), bbox_inches='tight', format=file_format)
+	plt.show()
+
 # %% Main flow
 def main():
 	analysis = []
@@ -105,18 +177,24 @@ def main():
 	synonym_test = get_synonym_test()
 	#Batch Task1 and Task 2 together.
 	for model_name in model_names:
+		print(f"Currently on {model_name}...")
 		wv = get_model(model_name)
 		detail, performance = details_analysis(synonym_test, wv, model_name)
 
 		#Step 1, output details
+		print(f"Outputting details of {model_name}'s performance...")
 		filename = f"{model_name}-details.csv"
 		output_csv(filename, detail)
 		analysis.append(performance)
 
 	#Batch Step 2, output analysis
+	print(f"Outputting analysis of all models...")
 	filename = "analysis.csv"
 	output_csv(filename, analysis)
 
+	print(f"Generating analysis graph...")
+	generateAnalysisBarGraph(analysis)
+	print("Done")
 
 if __name__ == "__main__":
     begin_time = time.perf_counter()
